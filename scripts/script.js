7f258eb9a6aae1916f9786890c09431fa6f8e3e1
Cam <cam.schnackel@gmail.com>
2017-09-04 19:41:15 -0500

    Corrected Hard Mode issue

--- scripts/script.js
+++ scripts/script.js
@@ -12,21 +12,7 @@ var mrc = 0;
 function submission() {
     console.log('button working');
     new employeeData($('#firstIn').val(), $('#lastIn').val(), $('#eNoIn').val(), $('#jobTitleIn').val(), $('#salaryIn').val());
-    /*
-    $("#dataTable > #dataBody:last-child").append("<tr>");
-    $("#dataTable > #dataBody:last-child").append("<td><span id='firstNameData'>");
-    $('#firstNameData').replaceWith(employeeArray[employeeArray.length-1].firstName);
-    $("#dataTable > #dataBody:last-child").append("<td><span id='lastNameData'>");
-    $('#lastNameData').replaceWith(employeeArray[employeeArray.length - 1].lastName);
-    $("#dataTable > #dataBody:last-child").append("<td><span id='eNoData'>");
-    $('#eNoData').replaceWith(employeeArray[employeeArray.length - 1].eNo);
-    $("#dataTable > #dataBody:last-child").append("<td><span id='jobTitleData'>");
-    $('#jobTitleData').replaceWith(employeeArray[employeeArray.length - 1].jobTitle);
-    $("#dataTable > #dataBody:last-child").append("<td><span id='salaryData'>");
-    $('#salaryData').replaceWith(employeeArray[employeeArray.length - 1].salary);
-    $("#dataTable > #dataBody:last-child").append("<td><span id='deleteEmployee'>");
-    $('#deleteEmployee').replaceWith("<button class='deleteMe'>Delete Employee</button>");
-    */
+
     $("#dataTable > #dataBody:last-child").append("<tr><td><span id='firstNameData'><td><span id='lastNameData'><td><span id='eNoData'><td><span id='jobTitleData'><td><span id='salaryData'><td><span id='deleteEmployee'>");
     $('#deleteEmployee').replaceWith("<button class='deleteMe'>Delete Employee</button>");
     $('#firstNameData').replaceWith(employeeArray[employeeArray.length - 1].firstName);
@@ -34,11 +20,16 @@ function submission() {
     $('#eNoData').replaceWith(employeeArray[employeeArray.length - 1].eNo);
     $('#jobTitleData').replaceWith(employeeArray[employeeArray.length - 1].jobTitle);
     $('#salaryData').replaceWith(employeeArray[employeeArray.length - 1].salary);
-    mrc += employeeArray[employeeArray.length - 1].salary/12;
+    mrc = calcMonthly(employeeArray[employeeArray.length - 1].salary);
+    console.log('Monthly cost for this employee is ' + mrc);
+    var mcd = parseInt($('#monthlyCostData').text());
+    console.log('Current Monthly cost is ' + mcd);
+    $('#monthlyCostData').text(mcd + mrc);
+    /* employeeArray[employeeArray.length - 1].salary/12;
     mrc *= 100;
     mrc = Math.round(mrc);
-    mrc /= 100;
-    $('#monthlyCostData').text(mrc);
+    mrc /= 100; */
+    //$('#monthlyCostData').text(mrc + $('#monthlyCostData').val());
     $('#firstIn').val('');
     $('#lastIn').val('');
     $('#eNoIn').val('');
@@ -57,5 +48,18 @@ function employeeData(firstIn, lastIn, eNoIn, jobTitleIn, salaryIn) {
 };
 
 function deleteEmployee() {
-    $(this).parent().parent().remove()
+    var salaryDeduction = ($(this).parent().prev().text());
+    $('#monthlyCostData').text(parseInt($('#monthlyCostData').text()) - parseInt(calcMonthly(salaryDeduction)));
+    $(this).parent().parent().remove();
 }
+
+function calcMonthly(value) {
+    MonthlyAmt = 0;
+    MonthlyAmt += parseInt(value) / 12;
+    MonthlyAmt *= 100;
+    MonthlyAmt = Math.round(MonthlyAmt);
+    MonthlyAmt /= 100;
+    return MonthlyAmt;
+}
+
+console.log('Function Test, 10000 monthly is ' + calcMonthly(10000));
